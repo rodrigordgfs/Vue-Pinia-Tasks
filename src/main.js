e48c7d8964ccfa28d8874e190b82b3be4aa25fc1
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2023-10-03 23:49:38 -0300

    Feature: Configurado o pinia

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import './styles/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+createApp(App).use(createPinia()).mount('#app')
