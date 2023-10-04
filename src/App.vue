a1f7111a8071608e0ccb0e84512351556b3a3dc3
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2023-10-03 23:38:55 -0300

    Feature: Projeto iniciado

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>teste</div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
